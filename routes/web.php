80a42abc6ae4fb76adf4413e8b74b1adc28035c7
ProbZap <107599648+Probzap@users.noreply.github.com>
2025-10-10 09:20:55 +0700

    Add profil admin dashboard and full CRUD controller

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/profils', [ProfilController::class, 'index'])->name('profils.index');
-Route::post('/profils', [ProfilController::class, 'store'])->name('profils.store');
+Route::redirect('/', '/profils');
+
+Route::resource('profils', ProfilController::class)
+    ->names('profils')
+    ->except(['show']);
